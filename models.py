18f78957fce94e0457e5a24992f227757ee3ed82
Shakthi Sathish <shakthi.s.2024.aiml@rajalakshmi.edu.in>
2025-12-23 14:20:55 +0530

    Updated models.py with latest changes

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel
 
 class Student(BaseModel):
- 
+    student_id: str
     name: str
     age: int
-    dob: str
     email: str
     department: str
-    year: int
+    gender: str
